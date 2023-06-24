a5b1a98dc047b2058e33a24ae5731835ac47060f
anton <afevralevtotyt@gmail.com>
2023-06-24 12:38:59 +0200

    Изменил webcofig, убрал аннотации свагер

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -1,11 +1,6 @@
 package ru.skypro.homework.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,12 +16,7 @@ import java.util.ArrayList;
 @RequestMapping("/ads")
 public class AdsController {
 
-    @Operation(summary = "Получить все объявления")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200",
-                    description = "ОК",
-                    content = {@Content(mediaType = "application/json")}),
-    })
+
     @GetMapping()
     public ResponseEntity<?> getAllAds() {
         AdsGetResp resp = new AdsGetResp();
@@ -34,49 +24,18 @@ public class AdsController {
         return ResponseEntity.ok().body(resp);
     }
 
-    @Operation(summary = "Получить информацию об объявлении")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200",
-                    description = "ОК",
-                    content = {@Content(mediaType = "application/json")}),
-            @ApiResponse(responseCode = "401",
-                    description = "Unauthorized"),
-            @ApiResponse(responseCode = "404",
-                    description = "Not found")
-    })
     @GetMapping("{id}")
     public ResponseEntity<?> getAds(@PathVariable Integer id) {
         ExtendedAdDTO extendedAdDTO = new ExtendedAdDTO();
         return ResponseEntity.ok().body(extendedAdDTO);
     }
 
-    @Operation(summary = "Добавить оъявление")
-    @ApiResponses({
-            @ApiResponse(responseCode = "201",
-                    description = "Created",
-                    content = {@Content(mediaType = "application/json")}),
-            @ApiResponse(responseCode = "401",
-                    description = "Unauthorized")
-    })
     @PostMapping()
     public ResponseEntity<?> addAd(@RequestBody AdsAddReq req) {
         AdDTO adDTO = new AdDTO();
         return ResponseEntity.status(HttpStatus.CREATED).body(adDTO);
     }
 
-    @Operation(summary = "Удалить объявление")
-    @ApiResponses({
-            @ApiResponse(responseCode = "204",
-                    description = "No content"
-            ),
-            @ApiResponse(responseCode = "401",
-                    description = "Unauthorized"),
-            @ApiResponse(responseCode = "403",
-                    description = "Forbidden"),
-            @ApiResponse(responseCode = "404",
-                    description = "Not found"
-            )
-    })
     @DeleteMapping("{id}")
     public ResponseEntity<?> removeAd(@PathVariable Integer id) {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
