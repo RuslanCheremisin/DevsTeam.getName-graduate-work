20c811c78620671883d6a501921ba9397539d963
afevralevtotyt <afevralevtotyt@gmail.com>
2023-06-27 13:49:58 +0200

    убрал лишние аннотации в классе UserDTO

--- src/main/java/ru/skypro/homework/dto/UserDTO.java
+++ src/main/java/ru/skypro/homework/dto/UserDTO.java
@@ -1,12 +1,9 @@
 package ru.skypro.homework.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
     int id;
     String email;
